66b6327cb94edfe3dc9ecc499aa41f5bbc371ce9
AntoninoRando <antonino.rando.2003@gmail.com>
2022-05-19 21:18:32 +0200

    Controller update

--- Controllers/AIController.java
+++ Controllers/AIController.java
@@ -1,7 +1,7 @@
 package Controllers;
+
 import CardsTools.Card;
 import CardsTools.Hand;
-import GameTools.Player;
 
 public class AIController extends Controller {
     // CONSTRUCTORS
@@ -12,30 +12,34 @@ public class AIController extends Controller {
     // METHODS
     @Override
     public void makePlay() {
-        int index = getPlay();
+        int[] indices = getPlay();
 
-        if (index == -1) {
-            drawFromDeck();
-            System.out.println(bringer.getNickname() + " drew 1 card.");
-            return;
+        for (int i : indices) {
+            if (i == -1) {
+                drawFromDeck();
+                System.out.println(bringer.getNickname() + " drew 1 card.");
+                break;
+            }
+
+            // !Questa parte sotto funziona solo perché sarò un solo elemento, altrimenti
+            // gli indici si spostano
+            Card toPlay = bringer.getHand().remove(i);
+            game.playCard(toPlay);
+            System.out.println(bringer.getNickname() + " played " + toPlay);
         }
-
-        Card toPlay = bringer.getHand().remove(index);
-        game.playCard(toPlay);
-        System.out.println(bringer.getNickname() + " played " + toPlay);
     }
 
     // !La best play dovrebbe considerare tanttissime cose, le carte giocate e le
     // carte degli avversari. Ad esempio se l'avversario pesca su giallo, se hai un
     // cambio giro giallo è bene giocarlo per far (probabilmente) ripescare
     // l'avversario
-    public int getPlay() {
+    public int[] getPlay() {
         Hand hand = bringer.getHand();
         for (int i = 0; i < hand.getSize(); i++) {
             if (hand.getCard(i).isPlayable(game)) {
-                return i;
+                return new int[] { i };
             }
         }
-        return -1;
+        return new int[] { -1 };
     }
 }
