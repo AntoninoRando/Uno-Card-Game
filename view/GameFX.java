5077596d22edb7afe89921a8137d19f0213d2886
AntoninoRando <antonino.rando.2003@gmail.com>
2022-07-07 19:55:30 +0200

    Fixed the launch.json for JavaFx

--- view/GameFX.java
+++ view/GameFX.java
@@ -1,7 +1,25 @@
 package view;
 
+
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class GameFX extends Application {
+    private Parent createContent() {
+        return new StackPane(new Text("Hello World"));
+    }
     
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(createContent(), 300, 300));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
