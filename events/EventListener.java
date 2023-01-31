88f5de75eeeac3e7908119a52f7a0bc277a69f40
AntoninoRando <75914073+AntoninoRando@users.noreply.github.com>
2023-01-31 15:31:45 +0100

    Improved comments

--- events/EventListener.java
+++ events/EventListener.java
@@ -4,23 +4,25 @@ import java.util.HashMap;
 
 /**
  * Any class implementing this interface is able to change its state when
- * notified from outside.
+ * notified from outside. To perform the change, this class must be notified
+ * about the type of event that occurred and the data associated. The changes
+ * must be implemented on the <code>update</code> method.
  */
 public interface EventListener {
-    default void update(Event event, HashMap<String, Object> data) {
-        throwUnimplementedError(event);
-    }
-
-    default int getEventPriority(Event event) {
-        return 1;
-    }
+    /**
+     * Changes the internal state of this based on an external event.
+     * 
+     * @param event The type of event that this interface should listen to.
+     * @param data  The data used to gather information about the external event.
+     */
+    abstract void update(Event event, HashMap<String, Object> data);
 
     /**
-     * Method to run inside the @overrited update method in case the EventListener
-     * didn't implemented the given eventType associated with the given Object data.
+     * Method to run inside the <code>update</code> method in case this didn't
+     * implemented the given event associated with the given Object data.
      * 
-     * @param event
-     * @param data
+     * @param event The eventlistened but not implemented.
+     * @param data  The data associated to the event not expected.
      * @throws Error
      */
     default void throwUnsupportedError(Event event, Object data) throws Error {
@@ -29,18 +31,24 @@ public interface EventListener {
     }
 
     /**
-     * Method to run inside the @overrited update method in case the EventListener
-     * didn't implemented the given eventType.
+     * Sets the priority of the events.
      * 
-     * @param event
-     * @param data
-     * @throws Error
+     * @param event The event notified.
+     * @return The priority of the event notified; the bigger the number, the
+     *         greater is the priority.
      */
-    default void throwUnimplementedError(Event event) throws Error {
-        throw new Error(this + " was notified about " + event + ", but it does not support this event!");
+    default int getEventPriority(Event event) {
+        return 1;
     }
 
-    default int compareTo(Event event, EventListener otherListener) throws Error {
+    /**
+     * Used to compare two eventListeners' event priority.
+     * 
+     * @param event         The same type of event.
+     * @param otherListener The other listener used to perform the comparison.
+     * @return An integer that tells which event listener as a greater priority.
+     */
+    default int compareTo(Event event, EventListener otherListener) {
         return -Integer.compare(getEventPriority(event), otherListener.getEventPriority(event));
     }
 }
